e269c238a3e9d7fc48015bf2d3a7601e4b253c7b
omid.khodatars <omid.khodatars@gmail.com>
2021-04-08 12:02:10 +0200

    mock the data layer for testing the service layer

--- src/main/java/com/learning/studentservice/StudentRepo.java
+++ src/main/java/com/learning/studentservice/StudentRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepo extends JpaRepository<Student, Long> {
-    Student getStudentByName(String student);
+    Student getStudentByName(String name);
 
     @Query("select avg (grade) from Student where active=true")
     Double getAvgGradeForActiveStudents();
